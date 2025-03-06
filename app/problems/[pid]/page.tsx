a1248dc313f845564176474c6490672bb7c652d6
UmarSkh <umar040903@gmail.com>
2025-03-06 13:00:12 +0530

    fix

--- app/problems/[pid]/page.tsx
+++ app/problems/[pid]/page.tsx
@@ -33,7 +33,7 @@ const Page = () => {
 
   const fetchData = useCallback(async () => {
     try {
-      const r = await fetch("https://atcb-backend.onrender.com/", {
+      const r = await fetch("https://atcb-root-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
