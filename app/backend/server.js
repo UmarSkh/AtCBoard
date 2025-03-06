a145cc514daace3b7a35f9ae33a0c8e783d5c022
UmarSkh <umar040903@gmail.com>
2025-03-06 14:12:47 +0530

    fix

--- app/backend/server.js
+++ app/backend/server.js
@@ -49,7 +49,11 @@ app.post('/', async (req, res) => {
     }
 
     async function getDynamicHTML(url) {
-      const browser = await puppeteer.launch();
+    //   const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      });
       const page = await browser.newPage();
       await page.goto(url, { waitUntil: 'networkidle2' });
       const html = await page.content();
