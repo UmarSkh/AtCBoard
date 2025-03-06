ceea989bd21857e0f9a3547b5e904b538a220d88
UmarSkh <umar040903@gmail.com>
2025-03-06 12:50:31 +0530

    Add Access Control

--- app/backend/server.js
+++ app/backend/server.js
@@ -11,6 +11,13 @@ const port = process.env.PORT || 3333
 app.use(bodyParser.json())
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 
 
 app.get('/', (req, res) => {
