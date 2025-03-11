d1c4e563cc00b5ba0c7e3fdfd453046412966fc6
UmarSkh <umar040903@gmail.com>
2025-03-11 11:29:19 +0530

    Restore fix

--- app/backend/server.js
+++ app/backend/server.js
@@ -71,7 +71,7 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
    
 
